c2a628502ca003f1f6f10a490d237f04e0e42716
mauroczf <mauroczf@gmail.com>
2021-10-21 17:41:31 -0400

    update deletar

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,13 +1,21 @@
 <template>
 <div class="row">
   <div class="q-pa-md items-start q-gutter-md"  v-for="(livros, index) in livros" :key="index">
+    <q-card-actions align="right">
+      </q-card-actions>
     <q-card class="my-card">
       <img src="https://www.guia55.com.br/wp-content/uploads/2019/10/48670720_m.jpg">
       <q-card-section>
         <div class="text-h6">{{livros.titulo}}</div>
         <div class="text-subtitle2">{{livros.autor}}</div>
         <div class="text-subtitle3">{{livros.desc}}</div>
+        <div class="text-subtitle3">{{livros.id}}</div>
       </q-card-section>
+      <q-card-actions align="right">
+        <q-btn flat round color="red" icon="delete" class="deletar" @click="deletar(livros.id)"/>
+        <q-btn flat round color="dark-grey" icon="edit" />
+        <!-- <q-btn flat round color="primary" icon="share" /> -->
+      </q-card-actions>
     </q-card>
   </div>
 </div>
@@ -25,6 +33,17 @@ export default {
   },
 
   methods:{
+    deletar(id) {
+      axios
+      .delete(`http://127.0.0.1:8000/api/v1/livros/${id}`)
+      .then((res) => {
+        this.getLivros()
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      alert("Item apagado com sucesso, atualize sua página")
+    },
     getLivros() {
       axios
       .get("http://127.0.0.1:8000/api/v1/livros")
