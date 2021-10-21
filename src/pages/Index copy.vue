6ee1755e609e4f63d414706a659c06743531e202
mauroczf <mauroczf@gmail.com>
2021-10-21 16:39:35 -0400

    Update 3.0

---
+++ src/pages/Index copy.vue
@@ -0,0 +1,88 @@
+<template>
+<div class="row">
+  <div class="q-pa-md items-start q-gutter-md"  v-for="(produtos, index) in produtos" :key="index">
+    <q-card class="my-card">
+      <img src="https://thumbs.dreamstime.com/b/no-image-available-icon-photo-camera-flat-vector-illustration-132483141.jpg">
+      <q-card-section>
+        <div class="text-h6">{{produtos.titulo}}</div>
+        <div class="text-subtitle2">{{produtos.desc}}</div>
+      </q-card-section>
+    </q-card>
+  </div>
+</div>
+</template>
+...
+<script>
+
+import axios from "axios";
+
+export default {
+  data () {
+    return {
+      produtos: [],
+      item: [
+        {
+          photo: "https://placeimg.com/500/300/nature?t=" + Math.random(),
+          type: "free",
+          desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+          titulo: "Titulo 1",
+        },
+        {
+          photo: "https://placeimg.com/500/300/nature?t=" + Math.random(),
+          type: "paid",
+          desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+          titulo: "Titulo 2",
+        },
+        {
+          photo: "https://placeimg.com/500/300/nature?t=" + Math.random(),
+          type: "free",
+          desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+          titulo: "Titulo 3",
+        },
+        {
+          photo: "https://placeimg.com/500/300/nature?t=" + Math.random(),
+          type: "free",
+          desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+          titulo: "Titulo 4",
+        },
+        {
+          photo: "https://placeimg.com/500/300/nature?t=" + Math.random(),
+          type: "paid",
+          desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+          titulo: "Titulo 5",
+        },
+        {
+          photo: "https://placeimg.com/500/300/nature?t=" + Math.random(),
+          type: "free",
+          desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+          titulo: "Titulo 6",
+        },
+      ],
+    }
+  },
+
+  methods:{
+    getProdutos() {
+      axios
+      .get("http://127.0.0.1:8000/api/v1/produtos")
+      .then((res) => {
+        this.produtos = res.data;
+        console.log(res.data)
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+    }
+  },
+  created() {
+    this.getProdutos()
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 200px
+  flex-direction: row
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,12 @@
 <template>
 <div class="row">
-  <div class="q-pa-md items-start q-gutter-md"  v-for="(produtos, index) in produtos" :key="index">
+  <div class="q-pa-md items-start q-gutter-md"  v-for="(livros, index) in livros" :key="index">
     <q-card class="my-card">
-      <img src="https://thumbs.dreamstime.com/b/no-image-available-icon-photo-camera-flat-vector-illustration-132483141.jpg">
+      <img src="https://www.guia55.com.br/wp-content/uploads/2019/10/48670720_m.jpg">
       <q-card-section>
-        <div class="text-h6">{{produtos.titulo}}</div>
-        <div class="text-subtitle2">{{produtos.desc}}</div>
+        <div class="text-h6">{{livros.titulo}}</div>
+        <div class="text-subtitle2">{{livros.autor}}</div>
+        <div class="text-subtitle3">{{livros.desc}}</div>
       </q-card-section>
     </q-card>
   </div>
@@ -19,55 +20,17 @@ import axios from "axios";
 export default {
   data () {
     return {
-      produtos: [],
-      item: [
-        {
-          photo: "https://placeimg.com/500/300/nature?t=" + Math.random(),
-          type: "free",
-          desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
-          titulo: "Titulo 1",
-        },
-        {
-          photo: "https://placeimg.com/500/300/nature?t=" + Math.random(),
-          type: "paid",
-          desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-          titulo: "Titulo 2",
-        },
-        {
-          photo: "https://placeimg.com/500/300/nature?t=" + Math.random(),
-          type: "free",
-          desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-          titulo: "Titulo 3",
-        },
-        {
-          photo: "https://placeimg.com/500/300/nature?t=" + Math.random(),
-          type: "free",
-          desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-          titulo: "Titulo 4",
-        },
-        {
-          photo: "https://placeimg.com/500/300/nature?t=" + Math.random(),
-          type: "paid",
-          desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-          titulo: "Titulo 5",
-        },
-        {
-          photo: "https://placeimg.com/500/300/nature?t=" + Math.random(),
-          type: "free",
-          desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-          titulo: "Titulo 6",
-        },
-      ],
+      livros: [],
     }
   },
 
   methods:{
-    getProdutos() {
+    getLivros() {
       axios
-      .get("http://127.0.0.1:8000/api/v1/produtos")
+      .get("http://127.0.0.1:8000/api/v1/livros")
       .then((res) => {
-        this.produtos = res.data;
-        console.log(res.data)
+        this.livros = res.data.livro;
+        console.log(res.data.livro)
       })
       .catch((error) => {
         console.log(error);
@@ -75,7 +38,7 @@ export default {
     }
   },
   created() {
-    this.getProdutos()
+    this.getLivros()
   }
 }
 </script>
